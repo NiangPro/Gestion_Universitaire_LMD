290ed706dbae14f611120961d2a6caa6765e6b7d
NiangPro <NiangProgrammeur@gmail.com>
2025-03-27 16:30:57 +0000

    refractory

--- app/Livewire/RapportPaiement.php
+++ app/Livewire/RapportPaiement.php
@@ -3,11 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 
+#[Title('Rapport Paiement')]
 class RapportPaiement extends Component
 {
+    #[Layout('components.layouts.app')]
     public function render()
     {
-        return view('livewire.rapport-paiement');
+        return view('livewire.paiement.rapport-paiement');
     }
 }
